fdfa1261d8f4a59048684fb2922cb4a2c00ae7ee
jnnan <13716095198@163.com>
2024-07-14 01:01:09 +0800

    first commit

---
+++ js/hidebib.js
@@ -0,0 +1,61 @@
+// from: http://www.robots.ox.ac.uk/~vedaldi/assets/hidebib.js
+function hideallbibs()
+{
+    var el = document.getElementsByTagName("div") ;
+    for (var i = 0 ; i < el.length ; ++i) {
+        if (el[i].className == "paper") {
+            var bib = el[i].getElementsByTagName("pre") ;
+            if (bib.length > 0) {
+                bib[0].style.display = 'none' ;
+            }
+        }
+    }
+}
+
+function togglebib(paperid)
+{
+    var paper = document.getElementById(paperid) ;
+    var bib = paper.getElementsByTagName('pre') ;
+    if (bib.length > 0) {
+        if (bib[0].style.display == 'none') {
+            bib[0].style.display = 'block' ;
+        } else {
+            bib[0].style.display = 'none' ;
+        }
+    }
+}
+
+function toggleblock(blockId)
+{
+   var block = document.getElementById(blockId);
+   if (block.style.display == 'none') {
+    block.style.display = 'block' ;
+   } else {
+    block.style.display = 'none' ;
+   }
+}
+
+function hideblock(blockId)
+{
+   var block = document.getElementById(blockId);
+   block.style.display = 'none' ;
+}
+
+function showblock(blockId)
+{
+   var block = document.getElementById(blockId);
+   block.style.display = 'block' ;
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+  var fixedBlock = document.querySelector('.fixed-block');
+  
+  if (scrollPosition > 200) {
+    fixedBlock.style.display = "block";
+  } else {
+    fixedBlock.style.display = "none";
+  }
+}
